85e1bd89dd5074af9644668d851fb2c6b6fb6375
Sanjay Subramanian <sanjayss@Sanjays-MacBook-Pro.local>
2022-12-08 15:44:04 -0800

    fixing compile errors in miner/worker

--- miner/worker.go
+++ miner/worker.go
@@ -806,7 +806,7 @@ func (w *worker) resultLoop() {
 				logs = append(logs, receipt.Logs...)
 			}
 			// Commit block and state to database.
-			_, err = w.chain.WriteBlockAndSetHead(block, receipts, logs, task.state, true)
+            _, err := w.chain.WriteBlockAndSetHead(block, receipts, logs, task.state, true)
 			if err != nil {
 				log.Error("Failed writing block to chain", "err", err)
 				continue
