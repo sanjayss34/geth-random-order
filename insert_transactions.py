f7773a57bb1d3c40b58b5f5e1f93ed8ece26fa26
Sanjay Subramanian <sanjayss@ras-22-40.vpn.berkeley.edu>
2022-12-17 08:16:00 +0530

    updating insert_transactions with max gas

--- insert_transactions.py
+++ insert_transactions.py
@@ -9,5 +9,6 @@ num_transactions = int(sys.argv[1])
 if __name__ == "__main__":
     w3 = Web3(Web3.IPCProvider('node1/geth.ipc'))
     for _ in tqdm(range(num_transactions)):
-        w3.eth.sendTransaction({'from': Web3.toChecksumAddress(w3.eth.accounts[0]), 'to': Web3.toChecksumAddress("0x8dd07043fb4e6237dc06abd42545527e3f828126"), 'value': w3.toWei(10, 'ether')})
+        w3.eth.sendTransaction({'from': Web3.toChecksumAddress(w3.eth.accounts[0]), 'to': Web3.toChecksumAddress("0x8dd07043fb4e6237dc06abd42545527e3f828126"),
+            'value': w3.toWei(10, 'ether'), 'gas': 21000})
         time.sleep(random.random()*0.2)
